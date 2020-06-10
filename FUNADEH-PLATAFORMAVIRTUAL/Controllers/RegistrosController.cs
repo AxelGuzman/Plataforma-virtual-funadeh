c7da3d8382f59c4a10c9b72129f1b549a0135dc8
AllanBennett27 <53162492+AllanBennett27@users.noreply.github.com>
2020-06-10 01:55:11 -0600

    Commit Allan

---
+++ FUNADEH-PLATAFORMAVIRTUAL/Controllers/RegistrosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FUNADEH_PLATAFORMAVIRTUAL.Controllers
+{
+    public class RegistrosController : Controller
+    {
+        // GET: Registros
+        public ActionResult Registros()
+        {
+            return View();
+        }
+    }
+}
